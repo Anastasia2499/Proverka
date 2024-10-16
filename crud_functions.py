6963978a54804a947f50b0de5c62421a5be5363d
Anastasia2499 <nastia2499@mail.ru>
2024-10-16 14:51:50 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -14,9 +14,14 @@ price INTEGER NOT NULL
 
 
 def initiate_db(title_, description_, price_):
-    cursor.execute("INSERT INTO Users (title, description, price) VALUES (?, ?, ?)", (title_, description_, price_))
-    connetion.commit()
+    check_initiate = cursor.execute('SELECT * FROM Users WHERE id == ?', (title_,))
+    if check_initiate.fetchone() is None:
+        cursor.execute("INSERT INTO Users (title, description, price) VALUES (?, ?, ?)",
+                   (f'Продукт:{title_}', f'Название:{description_}', price_))
+        connetion.commit()
+    return f'Название: Product {title_} | Описание: описание {description_} | Цена: {price_}'
+
 
 
 connetion.commit()
-connetion.close()
+# connetion.close()
