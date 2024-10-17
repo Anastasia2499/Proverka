f503c84ea5d728b46c3c67ea99b62e187b38a265
Anastasia2499 <nastia2499@mail.ru>
2024-10-17 12:31:29 +0300

    module_14_4.py

--- module_14_4.py
+++ module_14_4.py
@@ -8,7 +8,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from crud_functions import *
 
 
-api = "7939492635:AAFsuuDIDKunLgZvOEJt6a6CgR_bXMhGe8Q"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
